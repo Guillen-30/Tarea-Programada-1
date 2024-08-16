75656352400387518a414b9b11c2c4fc4805bab2
Sebas Guillen <sguillenguzman@gmail.com>
2024-08-16 14:11:33 -0600

    Agregar conexion a server local, integrar python con sql y python con html

--- TestPY/Conexion SQL.py
+++ TestPY/Conexion SQL.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 
 def conexion_sql_server():
     connection_string = 'mssql+pyodbc://sa:BasesTec@25.8.143.41/Tarea Programada 1?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes'
@@ -17,10 +20,25 @@ sql_query = "SELECT * FROM [dbo].[Empleado]"
 engine = conexion_sql_server()
 df = select_to_df(sql_query, engine)
 if df is not None:
-    print("Conexión y consulta exitosa.")
-    print("Primeras filas del DataFrame:")
-    print(df.head(40))  # Muestra las primeras 5 filas del DataFrame
-    print("\nResumen del DataFrame:")
-    print(df.info())  # Muestra un resumen de las columnas y el tipo de datos
+    print(df.to_json())
+    print(type(df.to_dict(orient='records')))
 else:
     print("No se pudo obtener el DataFrame.")
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/post-data', methods=['POST',"GET"])
+def post_data():
+    # Convert the DataFrame to a dictionary so it can be easily serialized to JSON
+    df = select_to_df(sql_query, engine)
+    
+    data = df.to_dict(orient='records')
+    print(f"Received data: {data}")
+    
+    # Return a response
+    return jsonify({"status": "success", "data_received": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
